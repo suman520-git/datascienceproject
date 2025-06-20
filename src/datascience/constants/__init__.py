1d1b35938162e6bbb368faa4590cb4e0872412d6
suman520-git <svemula520@gmail.com>
2025-06-21 00:51:24 +0530

    code updation

--- src/datascience/constants/__init__.py
+++ src/datascience/constants/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH=Path("config/config.yaml")
+CONFIG_FILE_PATH=Path("E:\datascienceproject\config\config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+
+# E:\datascienceproject\config\config.yaml
